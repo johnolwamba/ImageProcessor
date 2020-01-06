d4fec62ea56c3abc22e17e77911f1b8ee4f2e3b7
Johnstone Ananda <johnolwamba@gmail.com>
2020-01-06 09:42:01 +0300

    Initial Commit

---
+++ MindValleyiOSTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MindValleyiOSTest
+//
+//  Created by Johnstone Ananda on 06/01/2020.
+//  Copyright © 2020 MindValley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
